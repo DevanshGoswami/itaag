a919ab5c034f8e638dcbff24cf14c59826694144
Yash.S.Narang <y_narang@yahoo.in>
2021-11-21 11:39:01 +0530

    Added capability to download executables from home page

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,20 @@ import {
   Icon,
   useColorModeValue,
   createIcon,
-} from '@chakra-ui/react'
-import Head from 'next/head'
+} from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Home() {
+  const handleDownload = (type) => {
+    let a = document.createElement("a");
+    a.setAttribute("download", `${type}_executable.exe`);
+    if (type === "api") {
+      a.setAttribute("href", "/requestMaker.exe");
+    } else if (type === "service") {
+      a.setAttribute("href", "/serviceManager.exe");
+    }
+    a.click();
+  };
   return (
     <>
       <Head>
@@ -19,27 +29,52 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
           rel="stylesheet"
         />
-      <title>ITAAG.</title>
+        <title>ITAAG.</title>
       </Head>
 
-      <Container maxW={'3xl'}>
+      <Container maxW={"3xl"}>
         <Stack
           as={Box}
-          textAlign={'center'}
+          textAlign={"center"}
           spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}>
+          py={{ base: 20, md: 36 }}
+        >
           <Heading
             fontWeight={600}
-            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}>
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
             itaag.
           </Heading>
-          <Text color={'gray.500'}>
-            IT Automation Application Generator
-          </Text>
+          <Text color={"gray.500"}>IT Automation Application Generator</Text>
+          <Box>
+            <Button
+              bg={"blue.400"}
+              color={"white"}
+              type="submit"
+              _hover={{
+                bg: "blue.500",
+              }}
+              margin="10px"
+              onClick={() => handleDownload("api")}
+            >
+              Download API Automation EXE
+            </Button>
+            <Button
+              bg={"blue.400"}
+              color={"white"}
+              type="submit"
+              _hover={{
+                bg: "blue.500",
+              }}
+              margin="10px"
+              onClick={() => handleDownload("service")}
+            >
+              Download Service Automation EXE
+            </Button>
+          </Box>
         </Stack>
       </Container>
     </>
   );
 }
-
